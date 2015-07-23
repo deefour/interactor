5d834a7dcadec72154b012ab4c078f0d5616f8cf
Jason Daly <jason@deefour.me>
2015-07-23 10:23:24 -0400

    move docblocks to contract

--- src/Contract/Interactor.php
+++ src/Contract/Interactor.php
@@ -2,8 +2,18 @@
 
 namespace Deefour\Interactor\Contract;
 
+use App\Deefour\Interactor\Context;
+use App\Deefour\Interactor\Status\Failure;
+
 interface Interactor
 {
+    /**
+     * Getter for the context object bound to the interactor.
+     *
+     * @return Context
+     */
+    public function context();
+
     /**
      * Process the business logic this interactor exists to handle, using the
      * bound context for support.
@@ -14,4 +24,13 @@ interface Interactor
      * Rollback changes.
      */
     public function rollback();
+
+    /**
+     * Convenience method to fail the interactor, passing through to the Context.
+     *
+     * @param string $message [optional]
+     * @throws Failure
+     * @return void
+     */
+    public function fail($message = null);
 }
